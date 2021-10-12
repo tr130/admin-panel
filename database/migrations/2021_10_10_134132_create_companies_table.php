b021ee0778f00d09c52fe92a5df9c06ca5e9444e
Henry Golding <hgolding@protonmail.com>
2021-10-12 16:35:18 +0100

    created initial migrations, seeders and factories;

---
+++ database/migrations/2021_10_10_134132_create_companies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name', 250);
+            $table->string('email', 250)->nullable();
+            $table->string('logo', 250)->nullable();
+            $table->string('website', 250)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="/css/app.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body class="bg-gray-500">
+<header>
+    <nav class="p-6 bg-white flex justify-between mb-6">
+        <ul class="flex items-center">
+            <li>
+                <a href="/" class="p-3">Home</a>
+            </li>
+            <li>
+                <a href="/" class="p-3">Companies</a>
+            </li>
+            <li>
+                <a href="/" class="p-3">Employees</a>
+            </li>
+        </ul>
+        <ul class="flex items-center">
+            @auth
+                <li>
+                    <a href="" class="p-3">{{ auth()->user()->name }}</a>
+                </li>
+                <li>
+                    <form action="{{ route('logout') }}" method="POST" class="inline p-3">
+                        @csrf
+                        <button type="submit">Logout</button>
+                    </form>
+                </li>
+            @endauth
+
+            @guest
+                <li>
+                    <a href="" class="p-3">Login</a>
+                </li>
+                <li>
+                    <a href="" class="p-3">Register</a>
+                </li>
+            @endguest
+        </ul>
+    </nav>
+</header>
+@yield('content')
+</body>
+</html>
